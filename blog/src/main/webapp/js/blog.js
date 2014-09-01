6915aea94c60759ddf2da1844c3d82c11c65e3bf
Shinzul <Shinzul@Shinzul-PC>
2014-09-01 08:52:10 +0200

    Test view route and inclusions.

--- blog/src/main/webapp/js/blog.js
+++ blog/src/main/webapp/js/blog.js
@@ -1,10 +1,20 @@
-var blogApp = angular.module('blog', []);
+var blogApp = angular.module('blog', [ 'ngRoute' ]);
 
-blogApp.controller('CategoryController', function ($scope, $http)  {
-        $scope.categories = [];
-        
-        $http.get('services/category/all').success(function(data) {
-        	$scope.categories = data;
-         });
+blogApp.controller('NavigationController', function($scope, $http) {
+	$scope.categories = [];
+	$scope.title = "Programmaniaks";
+
+	$http.get('services/category/all').success(function(data) {
+		$scope.categories = data;
+	});
 });
 
+blogApp.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/index', {
+		templateUrl : 'partials/index.html'
+	}).when('/test', {
+		templateUrl : 'partials/test.html'
+	}).otherwise({
+		redirectTo : '/index'
+	});
+} ]);
